2086463851c55e0399319e29b3e6f6277c4dfd91
MikhailSorokin <sorokinmike@yahoo.com>
2019-03-20 13:10:22 -0400

    Attempt to add gulp for browser refresh, but it doesn't quite work yet. Hangups and freezes. New folder structure is set up, however.

---
+++ vrresume/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync').create();
+
+gulp.task('gulp_nodemon', function () {
+  nodemon({
+  script: './bin/www'           //this is where my express server is
+  , ext: 'js html css'          //nodemon watches *.js, *.html and *.css files
+  , env: { 'NODE_ENV': 'development' }
+  });
+});
+
+gulp.task('sync', function(){
+browserSync.init({
+port: 8001,                      //this can be any port, it will show our app
+proxy: 'http://localhost:8000/', //this is the port where express server works
+ui: {port: 8003},                //UI, can be any port
+reloadDelay: 1000                //Important, otherwise syncing will not work
+});
+gulp.watch(['./**/*.js', './**/*.html', './**/*.css']).on("change",
+browserSync.reload);
+});
+
+gulp.task('default', ['gulp_nodemon', 'sync']);
